ecc2f1a75f98e41fe8a5a141e79b211b4a1926eb
Erinthia07 <2231740005@student.polinema.ac.id>
2025-06-16 08:58:17 +0700

    perbaikan nama role

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -28,7 +28,7 @@ class RoleController extends Controller
     public function store(Request $request)
     {
         $validated = $request->validate([
-            'name' => 'required|unique:roles',
+            'name' => 'required|max:255',
             'description' => 'required|string',
         ]);
 
@@ -54,7 +54,7 @@ class RoleController extends Controller
         $role = Role::findOrFail($id);
 
         $validated = $request->validate([
-            'name' => 'required|unique:role,name,' . $role->id,
+            'name' => 'required|max:255',
             'description' => 'required|string',
         ]);
 
