5b87981bf31ba6379372996c84b0f7097d641b39
Erinthia07 <2231740005@student.polinema.ac.id>
2025-06-16 09:06:52 +0700

    perbaikan alert error saat login

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 use Illuminate\View\View;
 
 class AuthenticatedSessionController extends Controller
@@ -24,12 +25,20 @@ class AuthenticatedSessionController extends Controller
      */
     public function store(LoginRequest $request): RedirectResponse
     {
+    try {
         $request->authenticate();
 
         $request->session()->regenerate();
 
         return redirect()->intended(route('dashboard', absolute: false));
+        } catch (ValidationException $e){
+        return redirect()->back()
+            ->withInput($request->only('email'))
+            ->withErrors($e->errors()) // Untuk menampilkan error di form login
+            ->with('error', 'Invalid email or password. Please try again.');
     }
+}
+    
 
     /**
      * Destroy an authenticated session.
