3beacefde9904ef9ca9da610f57788bc9297ee4e
Erinthia07 <2231740005@student.polinema.ac.id>
2025-06-16 11:24:14 +0700

    perbaikan tabel

--- resources/views/livewire/users-menu.blade.php
+++ resources/views/livewire/users-menu.blade.php
@@ -9,7 +9,6 @@
             <p class="section-lead">In this section you can manage system user data such as adding, changing and
                 deleting</p>
             @if (session()->has('success'))
-                Add commentMore actions
                 <div class="alert alert-success alert-dismissible fade show" role="alert">
                     {{ session('success') }}
                     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
